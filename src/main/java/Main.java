126f59a272969d4783197f8934921bba16591349
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-06-05 16:05:22 -0500

    Maven Exercise

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter your heart's desire:\n");
+
+        Scanner input = new Scanner(System.in);
+        String userDesire = input.nextLine();
+        System.out.println("You entered: " + userDesire);
+        if (StringUtils.isNumeric(userDesire)){
+            System.out.println(userDesire + " is a numberrr");
+        } else {
+            System.out.println(userDesire + " is not a numberrr");
+        }
+        System.out.println("Your desire in swapCase: " + StringUtils.swapCase(userDesire));
+        System.out.println("Your string reversed: " + StringUtils.reverse(userDesire));
+    }
+}
