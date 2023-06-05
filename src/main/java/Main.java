d4cf38ced56391581039f7edfd4d716e0212d358
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-06-05 16:47:27 -0500

    clean Maven exercise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,9 @@
 import org.apache.commons.lang3.StringUtils;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Enter your heart's desire:\n");
-
         Scanner input = new Scanner(System.in);
         String userDesire = input.nextLine();
         System.out.println("You entered: " + userDesire);
